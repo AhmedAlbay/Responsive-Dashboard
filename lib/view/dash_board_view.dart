62db46fc5093d07da77da04f746b425989de851d
Your Name <you@example.com>
2024-02-14 18:59:38 +0200

    Add AdaptiveLayout

---
+++ lib/view/dash_board_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:responsivedashboard/widget/adaptive_layout.dart';
+import 'package:responsivedashboard/widget/dashboard_desktop_layout.dart';
+
+class DashBoardView extends StatelessWidget {
+  const DashBoardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileLAyout: (context) => const SizedBox(),
+        tabletLayout: (context) => const SizedBox(),
+        desktopLayout: (context) => const DashBoardDesktopLayout(),
+      ),
+    );
+  }
+}
---
+++ lib/widget/adaptive_layout.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AdaptiveLayout extends StatelessWidget {
+  const AdaptiveLayout(
+      {super.key,
+      required this.mobileLAyout,
+      required this.tabletLayout,
+      required this.desktopLayout});
+  final WidgetBuilder mobileLAyout, tabletLayout, desktopLayout;
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constains) {
+        if (constains.maxWidth < 600) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: mobileLAyout(context),
+          );
+        } else if (constains.maxWidth < 900) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: tabletLayout(context),
+          );
+        } else {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: desktopLayout(context),
+          );
+        }
+      },
+    );
+  }
+}
