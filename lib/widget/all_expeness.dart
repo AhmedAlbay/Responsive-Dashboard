72df63b1bfb77b1780be9f44f9dd3dd0e43403c3
Your Name <you@example.com>
2024-02-22 00:15:50 +0200

    refactor code

--- lib/widget/all_expeness.dart
+++ lib/widget/all_expeness.dart
@@ -1,35 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:responsivedashboard/utils/app_image.dart';
-import 'package:responsivedashboard/view/all_expeness_item_model.dart';
 import 'package:responsivedashboard/widget/all_expeness_header.dart';
-import 'package:responsivedashboard/widget/all_expeness_item.dart';
 import 'package:responsivedashboard/widget/all_expeness_item_list_view.dart';
+import 'package:responsivedashboard/widget/custom_background_container.dart';
 
 class AllExpeness extends StatelessWidget {
   const AllExpeness({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20),
-      child: Container(
-        padding: const EdgeInsets.all(20),
-        decoration: ShapeDecoration(
-          color: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
+    return const Padding(
+      padding: EdgeInsets.all(20),
+      child: CustomBackgroundContainer(
+        child: Column(
+        children: [
+          AllExpenessHeader(),
+          SizedBox(
+            height: 16,
           ),
-        ),
-        child: const Column(
-          children: [
-            AllExpenessHeader(),
-            SizedBox(
-              height: 16,
-            ),
           AllExpenessItemListView()
-          ],
-        ),
+        ],
+      ),
       ),
     );
   }
 }
+
--- lib/widget/all_expeness_item.dart
+++ lib/widget/all_expeness_item.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:responsivedashboard/utils/styles.dart';
 import 'package:responsivedashboard/view/all_expeness_item_model.dart';
-import 'package:responsivedashboard/widget/all_expeness_item_header.dart';
 import 'package:responsivedashboard/widget/inactive_and_active_all_expeness_item.dart';
 
 class AllExpenessItem extends StatelessWidget {
